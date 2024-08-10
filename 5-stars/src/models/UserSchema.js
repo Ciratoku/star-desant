98015395486824c4a4928f02034d45138828ed57
Ciratoku <edissonman255@gmail.com>
2024-08-10 13:05:58 +0300

    task, user schemas

---
+++ 5-stars/src/models/UserSchema.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  login: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+});
+
+module.exports = mongoose.model("User", UserSchema);
