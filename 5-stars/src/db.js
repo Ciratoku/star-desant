f275d2193e2e8fb3a7c0b8579ff0b7ff4312c0b5
Ciratoku <edissonman255@gmail.com>
2024-08-10 11:36:50 +0300

    db init

---
+++ 5-stars/src/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const mongoDB = process.env.DATABASE || "mongodb://127.0.0.1:27017/5-stars";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+module.exports = db;
