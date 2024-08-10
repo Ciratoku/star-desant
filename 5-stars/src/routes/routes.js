3f61828fb931b24328f447204d59d90e34f01003
Ciratoku <edissonman255@gmail.com>
2024-08-10 15:07:57 +0300

    taskcontroller in routes.js

--- 5-stars/src/routes/routes.js
+++ 5-stars/src/routes/routes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const UserController = require("../controllers/UserController.js");
+const TaskController = require("../controllers/TaskController.js");
+// Task
+router.post("/tasks", TaskController.CreateTask);
+router.get("/tasks/:id", TaskController.GetTask);
+router.delete("/tasks/:id", TaskController.DeleteTask);
+router.put("/tasks/:id", TaskController.UpdateTask);
 // User
 router.post("/users", UserController.CreateUser);
 router.get("/users/:login", UserController.GetUser);
 router.delete("/users/:login", UserController.DeleteUser);
 router.put("/users/:login", UserController.UpdateUser);
-// Task
+
 module.exports = router;
